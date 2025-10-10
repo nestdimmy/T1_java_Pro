e0f52b82862f828d40cefb49f5abd0911d680c00
Нестеров Дмитрий Андреевич <danesterov@vtb.ru>
2025-10-11 00:33:07 +0300

    homework one done

---
+++ src/main/java/TestClassT1.java
@@ -0,0 +1,55 @@
+import annotations.AfterSuite;
+import annotations.AfterTest;
+import annotations.BeforeSuite;
+import annotations.BeforeTest;
+import annotations.CsvSource;
+import annotations.Test;
+
+public class TestClassT1 {
+
+    @BeforeSuite
+    public static void beforeSuiteMethod() {
+        System.out.println("Я beforeSuiteMethod");
+    }
+
+    @AfterSuite
+    public static void afterSuiteMethod() {
+        System.out.println("Я afterSuiteMethod");
+    }
+
+
+    @BeforeTest
+    public void beforeTestActionOne() {
+        System.out.println("Я beforeTestActionOne");
+    }
+
+    @BeforeTest
+    public void beforeTestActionTwo() {
+        System.out.println("Я beforeTestActionTwo");
+    }
+
+    @Test(priority = 2)
+    public void test1() {
+        System.out.println("Я test1");
+    }
+
+    @Test(
+            priority = 12313,
+            arguments = @CsvSource("argument1,12345")
+    )
+    public void test2(String one, Integer two) {
+        System.out.println("Я test2, arg1: " + one + " arg2: " + two);
+
+    }
+
+    @AfterTest
+    public void afterTestActionOne() {
+        System.out.println("Я afterTestActionOne");
+    }
+
+    @AfterTest
+    public void afterTestActionTwo() {
+        System.out.println("Я afterTestActionTwo");
+    }
+
+}
---
+++ src/main/java/context/TestClassContext.java
@@ -0,0 +1,89 @@
+package context;
+
+import annotations.Test;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+public class TestClassContext {
+    private final List<Method> beforeTest = new ArrayList<>();
+    private final List<Method> afterTest = new ArrayList<>();
+    private final List<TestMethod> tests = new ArrayList<>();
+
+    private Method beforeSuite;
+    private Method afterSuite;
+
+    public void addTest(Method method) {
+        Test annotation = method.getAnnotation(Test.class);
+        if (annotation.priority() < 1 || annotation.priority() > 10) {
+            throw new IllegalArgumentException("Значение priority у @Test методов может быть от 1 до 10");
+        }
+
+        this.tests.add(new TestMethod(method));
+    }
+
+    public void addBeforeSuite(Method method) {
+        if (beforeSuite != null) {
+            throw new IllegalArgumentException("Только один метод с аннотацией @BeforeSuite" +
+                    " может быть представлен в рамках одного тест класа");
+        }
+
+        if (!Modifier.isStatic(method.getModifiers())) {
+            throw new IllegalArgumentException(
+                    "Аннотация @BeforeSuite может применяться только на статические методы"
+            );
+        }
+
+        this.beforeSuite = method;
+    }
+
+    public void addAfterSuite(Method method) {
+        if (afterSuite != null) {
+            throw new IllegalArgumentException("Только один метод с аннотацией @AfterSuite" +
+                    " может быть представлен в рамках одного тест класа");
+        }
+
+        if (!Modifier.isStatic(method.getModifiers())) {
+            throw new IllegalArgumentException(
+                    "Аннотация @AfterSuite может применяться только на статические методы"
+            );
+        }
+
+        this.afterSuite = method;
+    }
+
+    public void addBeforeTest(Method method) {
+        this.beforeTest.add(method);
+    }
+
+    public void addAfterTest(Method method) {
+        this.afterTest.add(method);
+    }
+
+    public Optional<Method> getBeforeSuite() {
+        return Optional.ofNullable(beforeSuite);
+    }
+
+    public Optional<Method> getAfterSuite() {
+        return Optional.ofNullable(afterSuite);
+    }
+
+    public List<Method> getBeforeTest() {
+        return beforeTest;
+    }
+
+    public List<Method> getAfterTest() {
+        return afterTest;
+    }
+
+    public List<TestMethod> getOrderedTests() {
+        return tests.stream()
+                .sorted(Comparator.comparingInt(TestMethod::getPriority))
+                .toList();
+    }
+}
